15eeaae0eddb292eb243cea857b08109c6c90358
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-09-24 11:14:47 +0200

    task 2 done

--- 02_update-and-store-item-values-in-a-list/js/components/createList.js
+++ 02_update-and-store-item-values-in-a-list/js/components/createList.js
@@ -1,9 +1,41 @@
+import { listKey } from "../settings.js";
+import { saveToStorage } from "../utils/storage.js";
+
 export default function createList(listItems) {
     const listContainer = document.querySelector("ul");
 
     listContainer.innerHTML = "";
 
     listItems.forEach(function (listItem) {
-        listContainer.innerHTML += `<li><input type="text" value="${listItem.item}" data-id="${listItem.id}" /></li>`;
+        listContainer.innerHTML += `
+          <li>
+            <input
+              type="text"
+              value="${listItem.item}"
+              data-id="${listItem.id}" />
+          </li>`;
     });
+
+    const textBoxes = document.querySelectorAll("li input[type=text]");
+    textBoxes.forEach(textBox => {
+      textBox.addEventListener("keyup", updateValue);
+    });
+
+    function updateValue(e) {
+      const id = e.target.dataset.id; // id
+      const value = e.target.value.trim(); // value
+      // find and update a given list item
+      const updatedList = updateValueInList(listItems, id, value);
+      saveToStorage(listKey, updatedList);
+    }
+}
+
+function updateValueInList(listItems, id, value) {
+  const itemIndex = listItems.findIndex(item => {
+    if (item.id === parseInt(id)) {
+      return true;
+    }
+  })
+  listItems[itemIndex].item = value;
+  return listItems;
 }
